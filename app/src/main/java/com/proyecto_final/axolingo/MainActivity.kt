28cfe020a29761d53b39ef229065d85bf4665dfb
Harb3rt0 <luisalbertohdzv09@gmail.com>
2025-10-19 23:08:10 -0600

    Vista de registro e inicio de sesion con intents

--- app/src/main/java/com/proyecto_final/axolingo/MainActivity.kt
+++ app/src/main/java/com/proyecto_final/axolingo/MainActivity.kt
@@ -1,18 +1,45 @@
 package com.proyecto_final.axolingo
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.proyecto_final.axolingo.forms.IniciarSesion
+import com.proyecto_final.axolingo.forms.Registrarse
 import com.proyecto_final.axolingo.views.MenuPrincipal
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var lienzo = MenuPrincipal(applicationContext)
-        setContentView(lienzo)
+        enableEdgeToEdge()
+        setContentView(R.layout.menu_bienvenida)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+
+        var btnRegistro: Button = findViewById(R.id.btnRegistro)
+        var btnLogin: Button = findViewById(R.id.btnIniciarSesion)
+
+        btnRegistro.setOnClickListener(evento)
+        btnLogin.setOnClickListener(evento)
     }
 
-    /*
-            setContentView(R.layout.)
-
-    * */
+    val evento = View.OnClickListener { view ->
+        when (view.getId()) {
+            R.id.btnRegistro -> {
+                val intent = Intent(this, Registrarse::class.java)
+                startActivity(intent)
+            }
+            R.id.btnIniciarSesion -> {
+                val intent = Intent(this, IniciarSesion::class.java)
+                startActivity(intent)
+            }
+        }
+    }
 }
